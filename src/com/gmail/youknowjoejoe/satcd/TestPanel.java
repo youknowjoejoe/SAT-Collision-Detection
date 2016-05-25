62beb633e722901e877b63f1a55f31642aca2558
Joseph <youknowjoejoe@gmail.com>
2016-05-24 17:37:03 -0700

    test commit when fixing elcipse workspace

--- src/com/gmail/youknowjoejoe/satcd/TestPanel.java
+++ src/com/gmail/youknowjoejoe/satcd/TestPanel.java
@@ -7,8 +7,6 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.geom.AffineTransform;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
